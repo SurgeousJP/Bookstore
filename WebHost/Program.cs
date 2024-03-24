db5faf2b31d6ca6bd0b70b8da5db268182b3eb1a
SurgeousJP <tuanbao2311@gmail.com>
2024-03-24 14:44:57 +0700

    Fix error can't access api from host running containers

--- WebHost/Program.cs
+++ WebHost/Program.cs
@@ -13,12 +13,4 @@ builder.Services.AddOcelot(builder.Configuration);
 var app = builder.Build();
 await app.UseOcelot();
 
-var channel = GrpcChannel.ForAddress("http://localhost:5183");
-var client = new Basket.API.gRPC.Basket.BasketClient(channel);
-var request = new GetBasketRequest();
-
-var response = await client.GetBasketAsync(request, new CallOptions());
-
-Console.WriteLine(response);
-
 app.Run();
