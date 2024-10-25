7c1ed8e553c94de8866cb6386191402fe56050af
SurgeousJP <tuanbao2311@gmail.com>
2024-10-25 19:52:18 +0700

    Enable HTTPS for server

--- src/APIGateway/Program.cs
+++ src/APIGateway/Program.cs
@@ -12,7 +12,7 @@ builder.Services
         options.AddPolicy("CorsPolicy", policy =>
         {
             policy
-                .WithOrigins("http://localhost:3000")
+                .WithOrigins("https://localhost:3000")
                 .AllowAnyMethod()
                 .AllowCredentials()
                 .AllowAnyHeader()
